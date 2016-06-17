b395f950a2f7d6edccacfd1d1a0b3c7ff88096f9
shatha shaath <shatha_shaath@hotmail.com>
2016-06-17 10:43:46 -0700

    set pizza image as a backhround cover

--- js/script.js
+++ js/script.js
@@ -1,37 +1,45 @@
-debugger;
 function Pizza(numberOfToppings, size) {
   this.numberOfToppings = numberOfToppings;
   this.size = size;
 }
 
 Pizza.prototype.calculate = function() {
-  if (this.size=="small"){
-    if (this.numberOfToppings <=1) {
+  if (this.size == "small") {
+    if (this.numberOfToppings <= 1) {
       return 10;
-    } else if (this.numberOfToppings ==2) {
+    } else if (this.numberOfToppings == 2) {
       return 11;
     } else {
       return 12;
     }
-  } else if (this.size=="medium") {
-      if (this.numberOfToppings <=1) {
-          return 15;
-    }else if (this.numberOfToppings == 2) {
+  } else if (this.size == "medium") {
+    if (this.numberOfToppings <= 1) {
+      return 15;
+    } else if (this.numberOfToppings == 2) {
       return 16;
     } else {
       return 17;
     }
-
-  }else if (this.size=="large") {
-    if (this.numberOfToppings <=1) {
-        return 20;
-    } else if (this.numberOfToppings==2) {
+  } else if (this.size == "large") {
+    if (this.numberOfToppings <= 1) {
+      return 20;
+    } else if (this.numberOfToppings == 2) {
       return 21;
-
     } else {
       return 22;
     }
   }
 
-
 };
+
+function readyFn() {
+  $('#calculate-button').click(function() {
+    var count = $("input:checkbox:checked").length;
+    var size = $("input[name=size]:checked").val();
+    var pizza = new Pizza(count, size);
+    var cost = pizza.calculate();
+    $('#cost').text("$"+parseInt(cost));
+  });
+}
+
+$(document).ready(readyFn);
