deac68b7fa2a1a6d0ef86fca7dea7a9deed668ef
shatha shaath <shatha_shaath@hotmail.com>
2016-06-17 09:20:47 -0700

    add pizza constructor and prototype

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ function Pizza(numberOfToppings, size) {
     this.numberOfToppings = numberOfToppings;
     this.size = size;
 }
+
+pizza.prototype.calculate = function() {
+    if (this.size=="small"){
+        if (this.numberOfToppings<=1) {
+            return 10;
+        } else if (this.numberOfToppings==2) {
+            return 11;
+        } else {
+            return 12;
+        }
+  }
+}
